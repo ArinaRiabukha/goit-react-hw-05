156ad0ba3542018452f59974cfe7760e171213d6
Arina Riabukha <arrin.uq@gmail.com>
2025-02-10 17:20:38 +0200

    MoviesPage change

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { searchMovies } from "../../api";
 import MovieList from "../../components/MovieList/MovieList";
@@ -6,39 +6,41 @@ import s from "./MoviesPage.module.css"
 
 const MoviesPage = () => {
     const [searchParams, setSearchParams] = useSearchParams();
-    const [query, setQuery] = useState(searchParams.get("query") || "");
-    const [movies, setMovies] = useState(() => {
-        const savedMovies = searchParams.get("movies");
-        return savedMovies ? JSON.parse(savedMovies) : [];
-    });
+    const queryParam = searchParams.get("query") || "";
+    
+    const [query, setQuery] = useState(queryParam);
+    const [movies, setMovies] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    const updateSearchParams = (key, value) => {
-        const updatedParams = new URLSearchParams(searchParams);
-        updatedParams.set(key, value);
-        setSearchParams(updatedParams);
-    };
+    useEffect(() => {
+        if (!queryParam) return; 
+        const fetchMovies = async () => {
+            setIsLoading(true);
+            try {
+                const result = await searchMovies(queryParam);
+                setMovies(result);
+            } catch (error) {
+                console.error("Error searching movies:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchMovies();
+    }, [queryParam]);
 
-    const handleSearch = async (e) => {
+    const handleSearch = (e) => {
         e.preventDefault();
-        setIsLoading(true);
-        try {
-            const result = await searchMovies(query);
-            setMovies(result);
-            updateSearchParams("query", query);
-            updateSearchParams("movies", JSON.stringify(result)); 
-        } catch (error) {
-            console.error("Error searching movies:", error);
-        } finally {
-            setIsLoading(false);
-        }
+        setSearchParams({ query }); 
     };
-
 
     return (
         <>
             <form onSubmit={handleSearch} className={s.container}>
-                <input value={query} onChange={(e) => setQuery(e.target.value)} className={s.search}/>
+                <input
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className={s.search}
+                />
                 <button type="submit" className={s.searchBtn}>Search</button>
             </form>
             <MovieList movies={movies} isLoading={isLoading} />
